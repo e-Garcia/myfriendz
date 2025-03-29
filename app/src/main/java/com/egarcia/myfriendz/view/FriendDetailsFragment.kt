753c90a1a9b6752c44390efda7cbaf36b00d7a8c
Erick Garcia <erick@egarcia.dev>
2025-03-29 02:27:15 -0700

    Added support to call a friend if the phone number is available.

--- app/src/main/java/com/egarcia/myfriendz/view/FriendDetailsFragment.kt
+++ app/src/main/java/com/egarcia/myfriendz/view/FriendDetailsFragment.kt
@@ -1,19 +1,22 @@
 package com.egarcia.myfriendz.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.egarcia.myfriendz.R
 import com.egarcia.myfriendz.databinding.FragmentFriendDetailsBinding
 import com.egarcia.myfriendz.editFriend.FriendDetailsFragmentDirections
+import com.egarcia.myfriendz.model.Friend
 import com.egarcia.myfriendz.viewmodel.FriendsDetailViewModel
 import com.egarcia.myfriendz.showFriend.view.FriendsListFragment
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.core.net.toUri
 
 /**
  * Responsible for displaying a friend contact details such as full name, how frequent should the
@@ -31,13 +34,14 @@ class FriendDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_friend_details, container, false)
+        dataBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_friend_details, container, false)
         return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FriendsDetailViewModel::class.java)
+        viewModel = ViewModelProvider(this)[FriendsDetailViewModel::class.java]
         arguments?.let {
             friendUuid = FriendDetailsFragmentArgs.fromBundle(it).friendUuid
         }
@@ -47,13 +51,29 @@ class FriendDetailsFragment : Fragment() {
             val action = FriendDetailsFragmentDirections.actionEditFriendFragment(friendUuid)
             findNavController().navigate(action)
         }
+        dataBinding.phoneDetail.setOnClickListener {
+            viewModel.friend.value?.let { friend ->
+                dialFriend(friend)
+            }
+        }
     }
 
     private fun observeViewModel() {
         viewModel.friend.observe(viewLifecycleOwner) { friend ->
             friend?.let {
                 dataBinding.friendDetails = it
+                dataBinding.phoneDetail.isEnabled = it.phone.isNotEmpty()
             }
         }
+    }
+
+    /**
+     * Handle the intent to prepopulate the dialer with the friend's phone number.
+     */
+    private fun dialFriend(friend: Friend) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = "tel:${friend.phone}".toUri()
+        }
+        startActivity(intent)
     }
 }
